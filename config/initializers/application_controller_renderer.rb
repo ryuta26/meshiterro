27ff86a49644b660289191fa35973f02add8c206
ryuta26 <fururyu0504@docomo.ne.jp>
2019-11-06 11:51:04 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+Refile.secret_key = '23294c97c528195b7f19b4eaa5c0dd150c6453c57d37a590bb04c54b1a7226edf96190a4297e13181d461d5ff5a5df2b38c062e86144de8f3bbe05dd6cfd8fd2'
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
